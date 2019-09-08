6e49444bb0aeba193bc1976d6fc3ad270a2ce7ad
Joao Pedro Taveira <joao.p.taveira@gmail.com>
2019-09-08 07:38:29 +0100

    Added 3.4 compatibility with patches found online

--- lib/advanced_roadmap/projects_helper_patch.rb
+++ lib/advanced_roadmap/projects_helper_patch.rb
@@ -4,12 +4,28 @@ module AdvancedRoadmap
   module ProjectsHelperPatch
     def self.included(base)
       base.class_eval do
+
         def project_settings_tabs_with_more_tabs
           tabs = project_settings_tabs_without_more_tabs
-          index = tabs.index({:name => 'versions', :action => :manage_versions, :partial => 'projects/settings/versions', :label => :label_version_plural})
-          if index
-            tabs.insert(index, {:name => "milestones", :action => :manage_milestones, :partial => "projects/settings/milestones", :label => :label_milestone_plural})
-            tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}     
+	  if User.current.allowed_to?(:manage_milestones, @project)
+	    options = {:name => 'versions', :action => :manage_versions,
+                       :partial => 'projects/settings/versions',
+                       :label => :label_version_plural}
+            index = tabs.index(options)
+            unless index # Needed for Redmine v3.4.x
+              options[:url] = {:tab => 'versions',
+                               :version_status => params[:version_status],
+                               :version_name => params[:version_name]}
+              index = tabs.index(options)
+            end
+            if index
+              tabs.insert(index + 1,
+                          {:name => 'milestones',
+                           :action => :manage_milestones,
+                           :partial => 'projects/settings/milestones',
+                           :label => :label_milestone_plural})
+              tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
+            end
           end
           return(tabs)
         end
